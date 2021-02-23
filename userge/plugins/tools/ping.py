fff4140585fdcd24a25511480dafd607c7363784
TomyPrs <tomy.prs25@gmail.com>
2021-02-23 18:08:06 +0700

    ping: add Telegram dc pinger
    Signed-off-by: TomyPrs <tomy.prs25@gmail.com>
    
    https://t.me/UserGeSpam/226973
    
    Signed-off-by: TomyPrs <tomy.prs25@gmail.com>
    
    finally
    
    Signed-off-by: TomyPrs <tomy.prs25@gmail.com>

--- userge/plugins/tools/ping.py
+++ userge/plugins/tools/ping.py
@@ -5,6 +5,7 @@
 # Please see < https://github.com/UsergeTeam/Userge/blob/master/LICENSE >
 #
 # All rights reserved.
+import requests
 
 from datetime import datetime
 
@@ -13,10 +14,17 @@ from userge import userge, Message
 
 @userge.on_cmd("ping", about={
     'header': "check how long it takes to ping your userbot",
-    'flags': {'-a': "average ping"}}, group=-1)
+    'flags': {'-t': "ping telegram dc following user dc"}}, group=-1)
 async def pingme(message: Message):
     start = datetime.now()
-    await message.edit('`Pong!`')
+    me_dc = (await message.client.get_me()).dc_id
     end = datetime.now()
     m_s = (end - start).microseconds / 1000
-    await message.edit(f"**Pong!**\n`{m_s} ms`")
+    tg_url = f"https://cdn{me_dc}.telesco.pe"
+    ping = round(requests.head(tg_url).elapsed.total_seconds() * 1000)
+    await message.edit('`Pong!`')
+    if '-t' in message.flags:
+    	await message.edit(f"**Pong!**\n`{ping} ms`")
+    else:
+    	await message.edit(f"**Pong!**\n`{m_s} ms`")
+    
